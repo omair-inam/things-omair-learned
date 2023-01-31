1ab9add6016edb61b8da1d8b9031989e704b07b0
Omair-Inam Abdul-Matin <omairkhawaja@gmail.com>
2023-01-30 21:21:00 -0500

    Update copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -131,7 +131,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Omair-Inam Abdul-Matin, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
