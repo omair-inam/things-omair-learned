eb09ea16ac6187f7b975c51d9f3f3b1e4e3b1a4c
Omair-Inam Abdul-Matin <omairkhawaja@gmail.com>
2023-01-30 21:20:41 -0500

    Update Twitter link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,7 +113,7 @@ const config = {
                  */
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/omair_inam',
               },
             ],
           },
